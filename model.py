d30ad812a31e1a13c54d93993bd847ac5a9826e1
r <r@rocket>
2025-03-06 17:39:11 -0500

    general structure of the GAN started

--- model.py
+++ model.py
@@ -1,7 +1,58 @@
-import torch
 import opendatasets as od
+import lightning as pl
+import torch
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+import os
+import torchvision
 
 
+class ArtDataset(pl.LightningDataModule):
+    def __init__(self):
+        raise NotImplementedError
 
-# Download the data
+    def train_dataloader(self):
+        raise NotImplementedError
+
+    def test_dataloader(self):
+        raise NotImplementedError
+
+    def val_dataloader(self):
+        raise NotImplementedError
+
+
+class ArtGenerator(torch.nn.Module):
+    def __init__(self):
+        raise NotImplementedError
+
+    def forward(self, x):
+        raise NotImplementedError
+
+
+class ArtDiscriminator(torch.nn.Module):
+    def __init__(self):
+        raise NotImplementedError
+
+    def forward(self, x):
+        raise NotImplementedError
+
+
+class ArtGAN(pl.LightningModule):
+    def __init__(self):
+        raise NotImplementedError
+
+    def forward(self, x):
+        raise NotImplementedError
+
+    def configure_optimizers(self, x):
+        raise NotImplementedError
+
+    def training_step(self, idx, batch_idx):
+        raise NotImplementedError
+
+    def validation_step(self, idx, batch_idx):
+        raise NotImplementedError
+
+# Download the data if not downloaded
 od.download("https://www.kaggle.com/datasets/ansonnnnn/historic-art")
+# Training loop
